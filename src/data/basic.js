32bfb2fa63793737f2c89d693c0685ed94feee76
unknown <bebus1998@chungbuk.ac.kr>
2020-09-02 23:54:52 +0900

    기본 베이스 작업(#1)
    - 더이상 불필요한 정보들 삭제(기존 web-skills 내용 들어있는 js파일들)
    - 알고리즘 데이터에 맞게 js파일 다시 구성(src/data/basic.js,
      src/data/advanced.js, src/data/codingtest.js)
    - src/data.js에서 필요없는 정보
      삭제하고 새 정보로 컬렉션 다시 구성

---
+++ src/data/basic.js
@@ -0,0 +1,55 @@
+export const basicCollection = {
+	name: "기초유형",
+	areas: [
+		{
+            name : "자료구조",
+			skills: [
+				{
+					name: "스택",
+					description: {
+						text: "Learn what accessibility is and why it is important.",
+						links: [
+							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
+							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
+							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"],
+							["Dev.to - Why Accessibility Matters", "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl"],
+							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"],
+							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
+							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"],
+						]
+					}
+				}                
+            ]
+        },{
+            name : "탐색",
+			skills: [
+				{
+					name: "DFS&BFS",
+					description: {
+						text: "Learn what accessibility is and why it is important.",
+						links: [
+							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
+							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
+							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"],
+							["Dev.to - Why Accessibility Matters", "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl"],
+							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"],
+							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
+							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"],
+						]
+					}
+				}
+            ]
+        },{
+			name : "정렬",
+			skills:[{
+				name:"버블정렬",
+				description:{
+						text : "설명",
+						ilnks:[
+							["google","https://www.google.com"]
+						]
+					}
+				}
+			]
+		}
+    ]}
---
+++ src/data/codingtest.js
@@ -0,0 +1,23 @@
+export const codingTestCollection={
+	name: "코딩테스트대비문제",
+	areas: [
+		{
+			skills: [
+				{
+					name: "스택",
+					description: {
+						text: "Learn what accessibility is and why it is important.",
+						links: [
+							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
+							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
+							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"],
+							["Dev.to - Why Accessibility Matters", "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl"],
+							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"],
+							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
+							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"],
+						]
+					}
+				}                
+            ]
+        }
+    ]}
