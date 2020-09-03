f829ea7dd4942bcacbf6c714e1725709434e051f
dungbik <q1w2e30630@gmail.com>
2020-09-03 20:49:05 +0900

    난이도 카테고리 사용 여부를 선택하는 설정 추가(#9)
    설명란에서 정의 혹은 예제에서는 난이도 카테고리가 필요없기 때문에
    사용여부를 설정할 수 있게 추가

--- src/data/basic.js
+++ src/data/basic.js
@@ -9,22 +9,18 @@ export const basicCollection = {
 				{
 					svg_name: "stack",
 					name: "스택",
-					category: 1,
+					category: 0,
 					description: {
 						text: "Learn what accessibility is and why it is important.",
-						links_1: [
+						links_0: [
 							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
 							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
-							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"]
-						],
-						links_2: [
+							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"],
 							["Dev.to - Why Accessibility Matters", "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl"],
-							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"]
-						],
-						links_3: [
+							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"],
 							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
 							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"]
-						]
+						],
 					}
 				}                
             ]
@@ -57,7 +53,7 @@ export const basicCollection = {
         },{
 			url: "sorting",
 			name : "정렬",
-			skills:[
+			skills: [
 				{
 					svg_name: "bubble",
 					name: "버블정렬",
