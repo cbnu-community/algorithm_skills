76e693b5ba4061b027ad707e770b83f7e49e42b9
ZScomnet <dark6ro@naver.com>
2020-09-05 06:11:45 +0900

    Design Collections (#4)

--- src/data/codingtest.js
+++ src/data/codingtest.js
@@ -1,30 +1,88 @@
 export const codingTestCollection = {
-	url: "coding test",
+	url: "coding-test",
 	name: "코딩테스트대비문제",
 	areas: [
 		{
+			url: "company",
+			name: "기업",
 			skills: [
 				{
-					svg_name: "stack",
-					name: "스택",
-					category: 1,
+					svg_name: "kakao",
+					name: "Kakao",
+					category: 0,
 					description: {
-						text: "Learn what accessibility is and why it is important.",
-						links_1: [
-							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
-							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
-							["Udacity - Chromevox Lite", "http://udacity.github.io/ud891/lesson3-semantics-built-in/02-chromevox-lite/"]
+						text: "카카오 블라인드 채용 및 기타 코딩테스트 대비 문제",
+						links_0: [
+							["2018 카카오 블라인드 - 비밀 지도","https://programmers.co.kr/learn/courses/30/lessons/17681"],
+							["2019 카카오 블라인드 - 실패율", "https://programmers.co.kr/learn/courses/30/lessons/42889"],
+							["2019 카카오 겨울 인턴십 - 크레인 인형뽑기 게임","https://programmers.co.kr/learn/courses/30/lessons/64061"],
+							["2020 카카오 인턴십 - 키패드","https://programmers.co.kr/learn/courses/30/lessons/67256"]
 						],
-						links_2: [
-							["Dev.to - Why Accessibility Matters", "https://dev.to/lhoffmanwg1/why-accessibility-matters-39nl"],
-							["Abilitynet - Why Accessibility Matters", "https://www.abilitynet.org.uk/why-accessibility-matters"]
-						],
-						links_3: [
-							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
-							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"]
-						]
 					}
-				}                
+				},
+				{
+					svg_name: "line",
+					name: "Line",
+					category: 0,
+					description: {
+						text: "Line 블라인드 채용 및 기타 코딩테스트 대비 문제",
+						links_0: [
+							["2019년 상반기 LINE 인턴 채용 코딩테스트 문제 및 해설","https://engineering.linecorp.com/ko/blog/2019-firsthalf-line-internship-recruit-coding-test/"],
+						],
+					}
+				},
+				{
+					svg_name: "naver",
+					name: "네이버",
+					category: 0,
+					description: {
+						text: "네이버의 경우 코딩테스트 문제를 공개하지 않아 후기를 위주로 참고",
+						links_0: [
+							["2019 네이버 코딩테스트 후기 -1","https://deepwelloper.tistory.com/122"],
+							["2019 네이버 코딩테스트 후기 -2", "https://programmers.co.kr/learn/courses/30/lessons/42889"],
+						],
+					}
+				},
+				{
+					svg_name: "samsung",
+					name: "삼성",
+					category: 0,
+					description: {
+						text: "삼성 소프트웨어 역량 테스트 기출 문제",
+						links_0: [
+							["백준 - 삼성 SW역량 테스트 기출문제","https://www.acmicpc.net/workbook/view/1152"],
+						],
+					}
+				},
+				{
+					svg_name: "toss",
+					name: "토스",
+					category: 0,
+					description: {
+						text: "토스의 경우 코딩테스트 문제를 공개하지 않아 후기를 위주로 참고",
+						links_0: [
+							["2020 토스 NEXT 개발자 채용 후기 -1","https://in0-pro.tistory.com/39"],
+							["2020 토스 NEXT 개발자 채용 후기 -2","https://torbjorn.tistory.com/521"]
+						],
+					}
+				},             
             ]
-        }
+        },
+        {
+        	url: "contest",
+        	name: "대회",
+			skills: [
+				{
+					svg_name: "acm-icpc",
+					name: "ACM-ICPC",
+					category: 0,
+					description: {
+						text: "ACM-ICPC 본선 문제집, 되도록 원문으로 된 문제를 풀어보는 것이 좋다.",
+						links_0: [
+							["백준 - ACM-ICPC 본선","https://www.acmicpc.net/workbook/view/3072"],
+						],
+					}
+				},             
+            ]
+        },
     ]}
